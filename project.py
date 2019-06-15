2d72da14b7aed4d5b809d3e992cc89215359ede7
BigBenLau <benlau413@gmail.com>
2019-06-15 14:53:26 +0800

    [junjieliu] change1

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 import numpy as np
 maxx = 10
 maxy = 10
-map1 = np.ones((maxx, maxy))*0
+c = ""
+map1 = np.ones((maxx + 1, maxy + 1))*0
 dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 def check_round_num(x, y):
@@ -10,7 +11,7 @@ def check_round_num(x, y):
         nx = x + dx[i]
         ny = y + dy[i]
         if(nx >= 0 and nx < maxx and ny >= 0 and nx < maxy):
-            if map_before[nx][ny]: count += 1
+            if map_before[nx][ny] == 1: count += 1
     return count
 
 def life_rule(map_before):
@@ -19,7 +20,9 @@ def life_rule(map_before):
         for j in range(maxy):
             if (check_round_num(i, j) <= 1 or check_round_num(i, j) >= 4): map1[i][j] = 0
             elif check_round_num(i, j) == 3: map1[i][j] = 1
-while 1:
+while c != "end":
+    c = input("input:")
+    map1[3][2] = map1[3][3] = map1[3][4] = 1
     map_before = map1[:]
-    print(map_before)
     life_rule(map_before)
+    print(map1)
