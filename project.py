358e474616738c78a5e96159403af16e4fc940a4
BigBenLau <benlau413@gmail.com>
2019-06-15 13:48:31 +0800

    [junjieliu] correct check_round_num

--- project.py
+++ project.py
@@ -1,12 +1,10 @@
 maxx = 10, maxy = 10
-map1[maxr][maxl]
+map1[maxx][maxy]
+map_before[maxx][maxy] = 
 dx = {-1, -1, -1, 0, 0, 1, 1, 1}, dy = {-1, 0, 1, -1, 1, -1, 0, 1}
 def check_round_num(x, y):
     count = 0
     for i in range(8): 
         nx = x + dx[i], ny = y + dy[i]
-        if(nx >= 0 && nx < maxx && ny >= 0 && nx < maxy && map1[maxr][maxl]) count = count + 1
+        if(nx >= 0 and nx < maxx and ny >= 0 and nx < maxy and map1[maxr][maxl]) count = count + 1
     return count
-
-def life_rule():
-    
